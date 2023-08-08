36a0d842a13a16634301177bc624a9626fb04415
roy-sandoval <roy@roysandoval.com>
2023-08-08 13:21:54 -0700

    Add Safe Web3Auth

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          black: "#061024",
+          sky: "#BBEEFF",
+          navy: "#2256BC",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
